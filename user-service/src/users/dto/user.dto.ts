05af4d10805534f468f2bab33f420ff64d3149c9
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-07-26 01:14:49 +0700

    feat: added user DTO for update user, and password change

---
+++ user-service/src/users/dto/user.dto.ts
@@ -0,0 +1,53 @@
+import { Exclude } from "class-transformer";
+import { IsOptional, IsString, MinLength } from "class-validator";
+
+export class UpdateUserDto {
+    @IsOptional()
+    @IsString()
+    name?: string
+
+    @IsOptional()
+    @IsString()
+    email?: string
+
+    @IsOptional()
+    @IsString()
+    phone?: string
+
+    @IsOptional()
+    @IsString()
+    photo?: string
+
+    @IsOptional()
+    @IsString()
+    position?: string
+}
+
+export class UserProfile {
+    id: string
+    name: string
+    email: string
+    phone?: string
+    photo?: string
+    position?: string
+    role: string
+    createdAt: Date
+    updatedAt: Date
+
+    @Exclude()
+    password: string
+}
+
+export class UpdatePasswordDto {
+    @IsString()
+    @MinLength(6)
+    currentPassword: string
+
+    @IsString()
+    @MinLength(6)
+    newPassword: string
+
+    @IsString()
+    @MinLength(6)
+    confirmPassword: string
+}
